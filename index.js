85bb9409175568bfc7a8ef498628511f674ed87e
jsacero90 <stiddark@gmail.com>
2022-06-05 18:47:46 -0500

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -2,12 +2,3 @@
 import './style.css';
 
 // Write Javascript code!
-
-const array = [1, 2, 3, 4];
-
-const appDiv = document.querySelector('#app');
-const listUl = document.createElement('ul');
-let listString = '';
-array.forEach((data) => (listString += `<li>${data}</li>`));
-listUl.innerHTML = listString;
-appDiv.appendChild(listUl);
